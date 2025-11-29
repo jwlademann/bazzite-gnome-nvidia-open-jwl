1926e540be22015fb17513f3551bc8426b20e2e3
jwlademann <james.lademann@landregistry.gov.uk>
2025-11-29 14:46:58 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux
+dnf5 -y install gparted
+dnf5 -y install blueman blueman-nautilus
 
 # Use a COPR Example:
 #
@@ -18,7 +20,12 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr enable lukenukem/asus-linux
+dnf5 -y install asusctl asusctl-rog-gui 
+dnf5 -y copr enable starfish/howdy-beta
+dnf5 -y install howdy howdy-gtk
+
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
