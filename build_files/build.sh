a004ffab8719c9599c976acb6f960b5316df0571
jwlademann <james.lademann@landregistry.gov.uk>
2026-03-12 01:14:48 +0000

    Add mbuffer installation to build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ set -ouex pipefail
 # dnf5 install -y tmux
 dnf5 -y install gparted
 dnf5 -y install blueman blueman-nautilus
-dnf5 -y install btrbk
+dnf5 -y install btrbk mbuffer
 
 # Use a COPR Example:
 #
